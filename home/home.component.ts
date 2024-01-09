bd20c37aff4b820624705b167980f7dcbbc944ec
showk.mahmoud <showk.mahmoud@3ddx.com>
2024-01-09 04:39:22 +0400

    NF: home page data

--- home/home.component.ts
+++ home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { data } from 'src/app/shared/Static Data/data';
+import { LOCAL_STORAGE_KEY } from 'src/app/shared/enums/localStorageKey';
+import { ITeam } from 'src/app/shared/interfaces/team';
+import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  teams: ITeam[] = [];
+  constructor(public localStorage:LocalStorageService){
 
+  }
+  ngOnInit(): void {
+    this.getTeams()
+  }
+
+  getTeams(){
+    if(this.localStorage.getItem(LOCAL_STORAGE_KEY.TEAMS)){
+    }else{
+      this.localStorage.setItem(LOCAL_STORAGE_KEY.TEAMS,data)
+    }
+    this.teams = this.localStorage.getItem(LOCAL_STORAGE_KEY.TEAMS);
+  }
 }
